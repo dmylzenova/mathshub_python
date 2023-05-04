96c0f17ba0f8d587ac904ec2f7d8c139832b4cb2
DariMylzenova <darimamylzenova@gmail.com>
2023-05-05 01:22:50 +0700

    rm token

--- 9_Project/bot/main.py
+++ 9_Project/bot/main.py
@@ -3,8 +3,7 @@ from currency_bot import run_currency_bot
 
 if __name__ == '__main__':
     # константа с вашим токеном, полученным из BotFather
-    TOKEN = "6011813145:AAF5avWYedNHKaozW7Pxe6LkshbtY48ityU"
-    # YOUR_TOKEN"
+    TOKEN = "YOUR_TOKEN"
 
     # запустить простого демо-бота
     # run_simple_bot(TOKEN)
